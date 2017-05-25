99305b01a6d9c12e8d87a211bfdb401bec19900e
liao <liao>
2017-05-25 12:21:42 +0800

    s

--- DAL/MyDbContext.cs
+++ DAL/MyDbContext.cs
@@ -8,7 +8,7 @@ namespace DAL
     {
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
-            
+             
         }
         public DbSet<Category> Category { get; set; }
         public DbSet<Article> Article { get; set; }
