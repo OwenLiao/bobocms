3336644176185ebf128979c3c3344ae823ddf019
liao <liao>
2017-06-06 15:00:21 +0800

    admin-home创建

--- Web/Areas/Admin/Controllers/HomeController.cs
+++ Web/Areas/Admin/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
         public IActionResult Index()
